e4ce7baf442a0509873256a1a080d91aa55af400
sarah77naser <90090577+sarah77naser@users.noreply.github.com>
2023-07-20 00:41:53 +0300

    HOLA

---
+++ decision tree/desicion-tree.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 14:57:36 2023
+
+@author: sara
+"""
+
+# Decision desicion tree
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold, cross_val_score
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, 2:3].values
+y = dataset.iloc[:, 4].values
+
+
+# Feature Scaling
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#sc_y = StandardScaler()
+#X_train = sc_X.fit_transform(X_train) 
+#X_test = sc_X.fit_transform(X_test) 
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+total_acc = 0
+for i in range(20):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+    
+    # Training the Decision Tree Regression model on the whole dataset
+    from sklearn.tree import DecisionTreeClassifier
+    classifire = DecisionTreeClassifier(criterion= "entropy")
+    classifire.fit(X_train, y_train)
+    
+    k_folds = KFold(n_splits = 5)
+
+    scores = cross_val_score(classifire, X, y, cv = k_folds)
+
+    
+    # Predicting the Test set results
+    y_pred = classifire.predict(X_test)
+    
+    #Making the confusion Matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+    acc =(cm[0][0]+cm[1][1])/len(y_test)
+    print('the accuracy of logistic regression classifier is ', acc*100, '%')
+    total_acc = total_acc+acc
+    
+final =total_acc/20
+print('averaga of accuracy of 20 times  ', (final*100), '%')
+print("Cross Validation Scores: ", scores)
+
+
---
+++ svm/SVM.py
@@ -0,0 +1,52 @@
+# Support Vector Machine (SVM)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+
+total_acc = 0
+for i in range(20):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state= 0)
+    
+    
+    # Feature Scaling
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    print(X_train)
+    print(X_test)
+    
+    # Training the SVM model on the Training set
+    from sklearn.svm import SVC
+    classifier = SVC(kernel = 'linear', random_state = 10)
+    classifier.fit(X_train, y_train)
+    
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix, accuracy_score
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    accuracy_score(y_test, y_pred)
+    
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+    ac=(cm[0][0]+cm[1][1])/len(y_pred)
+    print(ac)
+    print('the accuracy of logistic regression classifier is ', ac*100, '%')
+    total_acc = total_acc+ac
+    
+final =total_acc/20
+print('averaga of accuracy of 20 times  ', (final*100), '%')
